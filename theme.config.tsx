94e0a6b08a0cb36bddfff34d64851754bef9d528
Israel Antunes Neto <israneto20@gmail.com>
2024-10-18 11:24:17 -0300

    Versão V 0.0.0

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span><strong>DrugXpert Documentation</strong></span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
