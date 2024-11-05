5ad49304d05c3e48a82b3018197b7723a7a014c5
Israel Antunes Neto <62219211+isranetoo@users.noreply.github.com>
2024-11-05 10:48:57 -0300

    SMILES added

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { DocsThemeConfig } from 'nextra-theme-docs';
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Logo from './public/assets/logo.png';
 
@@ -30,12 +31,24 @@ const config: DocsThemeConfig = {
       </>
     ),
   },
+  head() {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      'https://drugxpert.net/' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'DrugXpert'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'DrugXpert'}
+        />
+      </>
+    );
+  },
 };
 
 export default config;
-
-
-
-// ![DrugXpert Interface](public/assets/logo.png)
-// ![Real-Time Preview](public/assets/molecula1.png)
-// ![Analytics Dashboard](public/assets/molecula2.png)
